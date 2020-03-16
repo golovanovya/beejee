0d8d87283b4514bf4b7b3aed57a9632c7ee5a074
Yury G <golovanovya@gmail.com>
2020-03-16 10:40:09 +0300

    edit authorization

--- public/index.php
+++ public/index.php
@@ -20,13 +20,14 @@ $request = Laminas\Diactoros\ServerRequestFactory::fromGlobals(
 $strategy = (new League\Route\Strategy\ApplicationStrategy)->setContainer($container);
 $router = (new League\Route\Router)->setStrategy($strategy);
 $router->map('GET', '/', [App\Controller\JobController::class, 'indexAction']);
-$router->map('GET', '/login', [App\Controller\SiteController::class, 'loginAction']);
+$router->map('GET', '/login', [App\Controller\SiteController::class, 'loginAction'])
+    ->middleware(new App\Middleware\Anon());
 $router->map('POST', '/login', [App\Controller\SiteController::class, 'loginAction']);
 $router->map('GET', '/logout', [App\Controller\SiteController::class, 'logoutAction']);
 $router->map('GET', '/create', [App\Controller\JobController::class, 'taskFormAction']);
 $router->map('POST', '/create', [App\Controller\JobController::class, 'createAction']);
 $router->map('GET', '/update/{id:number}', [App\Controller\JobController::class, 'taskFormAction'])
-    ->middleware(new \App\Middleware\Auth());
+    ->middleware(new App\Middleware\Auth());
 $router->map('POST', '/update/{id:number}', [App\Controller\JobController::class, 'updateAction']);
 $router->middleware(new Middlewares\AuraSession());
 
